6cbeba5a688b69547fdbe258b75eca3ae218bcbe
Tiago Vieira <tiago@tvieira.net>
2014-01-27 20:23:50 -0200

    Implementando o sistema de multi idioma

--- application/config/database.php
+++ application/config/database.php
@@ -48,12 +48,12 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = "localhost";
+$db['default']['username'] = "root";
+$db['default']['password'] ="";
+$db['default']['database'] = "Ala";
 $db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
+$db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
---
+++ application/language/portuguese-br/mensagem_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['discursos'] = 'Discursos';
+$lang['discurso'] = 'Discurso';
+$lang['nomeMembro'] = 'Nome do Membro';
+
+?>
